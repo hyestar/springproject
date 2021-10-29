4fecf92c62e8eb07e1d1120ad4dbb9b11b96d769
hyestar <0109jhs@naver.com>
2021-10-29 16:22:14 +0900

    회원가입시 입력 데이터 유효성 체크

--- src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
@@ -20,6 +20,24 @@ public class UsrMemberController {
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		if (loginId == null) {
+			return "loginId(을)를 입력해주세요.";
+		}
+		if (loginPw == null) {
+			return "loginPw(을)를 입력해주세요.";
+		}
+		if (name == null) {
+			return "name(을)를 입력해주세요.";
+		}
+		if (nickname == null) {
+			return "nickname(을)를 입력해주세요.";
+		}
+		if (cellphoneNo == null) {
+			return "cellphoneNo(을)를 입력해주세요.";
+		}
+		if (email == null) {
+			return "email(을)를 입력해주세요.";
+		}
 		
 		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 		if (id == -1) {
