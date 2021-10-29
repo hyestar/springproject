96afd385b0ebd34532d4a0821b33d77fabcf52fd
hyestar <0109jhs@naver.com>
2021-10-29 16:24:31 +0900

    회원가입시 입력된 데이터의 길이가 0인 경우 공백 체크

--- src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrMemberController.java
@@ -20,22 +20,22 @@ public class UsrMemberController {
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
-		if (loginId == null) {
+		if (loginId == null || loginId.trim().length() == 0) {
 			return "loginId(을)를 입력해주세요.";
 		}
-		if (loginPw == null) {
+		if (loginPw == null || loginPw.trim().length() == 0) {
 			return "loginPw(을)를 입력해주세요.";
 		}
-		if (name == null) {
+		if (name == null || name.trim().length() == 0) {
 			return "name(을)를 입력해주세요.";
 		}
-		if (nickname == null) {
+		if (nickname == null || nickname.trim().length() == 0) {
 			return "nickname(을)를 입력해주세요.";
 		}
-		if (cellphoneNo == null) {
+		if (cellphoneNo == null || cellphoneNo.trim().length() == 0) {
 			return "cellphoneNo(을)를 입력해주세요.";
 		}
-		if (email == null) {
+		if (email == null || email.trim().length() == 0) {
 			return "email(을)를 입력해주세요.";
 		}
 		
