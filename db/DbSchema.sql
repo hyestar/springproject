2a841bac699faa2b2bc74bda897b31b7754efde0
hyestar <0109jhs@naver.com>
2021-11-03 16:44:45 +0900

    게시물 테이블에 회원번호 칼럼 추가

--- db/DbSchema.sql
+++ db/DbSchema.sql
@@ -60,6 +60,14 @@ loginPw = 'user2',
 cellphoneNo = '01098769876',
 email = 'zxcvbnm@gmail.com';
 
+# 게시물 테이블에 회원정보 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
+
+# 기존 게시물의 작성자를 2번으로 수정
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
+
 SELECT * 
 FROM `member`;
 
