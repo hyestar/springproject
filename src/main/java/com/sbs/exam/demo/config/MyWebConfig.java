01b9a8d8cbbb9c098262c8c6e76029e8b6a67182
hyestar <0109jhs@naver.com>
2021-11-16 16:25:37 +0900

    BeforeAction 인터셉터 등록

---
+++ src/main/java/com/sbs/exam/demo/config/MyWebConfig.java
@@ -0,0 +1,25 @@
+package com.sbs.exam.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.sbs.exam.demo.interceptor.BeforeActionInterceptor;
+
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+
+	@Autowired
+	BeforeActionInterceptor beforeActionInterceptor;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+
+		InterceptorRegistration ir = registry.addInterceptor(beforeActionInterceptor);
+		ir.addPathPatterns("/**").excludePathPatterns("/resource/**");
+
+	}
+
+}
--- src/main/java/com/sbs/exam/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/sbs/exam/demo/interceptor/BeforeActionInterceptor.java
@@ -3,8 +3,10 @@ package com.sbs.exam.demo.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Component
 public class BeforeActionInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
