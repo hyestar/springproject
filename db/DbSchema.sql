c48964c82b18621cb112e0a04ad2002aa95a9b03
hyestar <0109jhs@naver.com>
2021-11-24 17:33:46 +0900

    게시물 페이징 준비(게시물 대량 추가)

--- db/DbSchema.sql
+++ db/DbSchema.sql
@@ -142,3 +142,13 @@ FROM article;
 
 SELECT * 
 FROM `board`;
+
+# 게시물 갯수 늘리기
+INSERT INTO article
+(
+    regDate, updateDate, memberId, boardId, title, `body`
+)
+SELECT NOW(), NOW(), FLOOR(RAND() * 3 + 1), FLOOR(RAND() * 2 + 1), CONCAT('제목_',RAND()), CONCAT('내용_',RAND())
+FROM article;
+
+SELECT FLOOR(RAND() * 2 + 1);
